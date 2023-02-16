5c01b04a0aba03ab74b64dc1a4f969242699e0e0
1588974 <111997896+1588974@users.noreply.github.com>
2023-02-16 13:22:30 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,38 @@
+/*
+Colby Freeman
+Period 1
+2/7/2023
+Reversing an array
+*/
+import java.util.Scanner;
+public class main { 
+   public static void main(String[] args) {
+     // try {
+         Scanner Strings = new Scanner(System.in);
+         System.out.println("Array Length: ");
+         int ArrayNum = Integer.parseInt(Strings.nextLine()); 
+         double[] Number = new double[ArrayNum];
+         int Target = 0;
+         while (Target < Number.length) {
+            System.out.println("[Element: " + Target + "] - Number");
+            double Object = Double.parseDouble(Strings.nextLine());
+            Number[Target] = Object;
+            Target += 1;
+         }
+         double Temp;
+         for (int i = 0; i < Number.length; i++) {
+            int Reverse = Number.length - (i + 1);
+            //System.out.println(Number.length + " | " + Reverse + " | " + i);
+            System.out.println(i + " | " + Reverse);
+            Temp = Number[Reverse];
+            Number[Reverse] = Number[i];
+            Number[i] = Temp;
+         }
+         for (double i : Number) {
+            System.out.println(i);
+         }
+    //  } catch (Exception e) {
+    //     System.out.println(e);
+    //  }
+   }
+}
